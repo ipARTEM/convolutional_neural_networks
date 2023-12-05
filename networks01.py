22201f728e8f3d0671669e583cf8ae4a9891f650
Artem Khimin <a.khimin@yandex.ru>
2023-12-05 16:07:20 +0300

    network

--- networks01.py
+++ networks01.py
@@ -1,5 +1,211 @@
 # Подключение модуля для загрузки данных из облака
 import gdown
 
+
+import zipfile
+
 # Загрузка zip-архива с датасетом из облака на диск виртуальной машины colab
-gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l5/middle_fmr.zip', None, quiet=True)
+# gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l5/middle_fmr.zip', None, quiet=True)
+
+# Разархивация датасета в директорию 'content/cars'
+# !unzip -qo "middle_fmr.zip" -d /content/cars
+#
+
+# Для работы с файлами
+import os
+
+from PIL import Image                     # Отрисовка изображений
+import random                             # Генерация случайных чисел
+import matplotlib.pyplot as plt           # Отрисовка графиков
+
+import numpy as np                        # Библиотека работы с массивами
+
+# Подключение нужных слоев из модуля tensorflow.keras.layers
+
+from tensorflow.keras.models import Sequential
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten,Dropout, BatchNormalization
+
+# Подключение оптимизатора Adam
+from tensorflow.keras.optimizers import Adam
+
+# %matplotlib inline
+
+file_zip=zipfile.ZipFile('middle_fmr.zip','r')
+file_zip.extractall('/content/cars/')
+file_zip.close()
+
+# # Папка с папками картинок, рассортированных по категориям
+IMAGE_PATH = '/content/cars/'
+
+print(os.listdir(IMAGE_PATH))
+
+# Определение списка имен классов
+CLASS_LIST = sorted(os.listdir(IMAGE_PATH))
+
+# Определение количества классов
+CLASS_COUNT = len(CLASS_LIST)
+
+# Проверка результата
+print(f'Количество классов: {CLASS_COUNT}, метки классов: {CLASS_LIST}')
+
+i = 1
+
+# Формирование пути к выборке одной марки авто
+print(f'{IMAGE_PATH}{CLASS_LIST[i]}/')
+
+for cls in CLASS_LIST:
+    print(cls, ':', os.listdir(f'{IMAGE_PATH}{cls}/'))
+
+
+# Создание заготовки для изображений всех классов
+fig, axs = plt.subplots(1, CLASS_COUNT, figsize=(25, 5))
+
+# Для всех номеров классов:
+for i in range(CLASS_COUNT):
+    # Формирование пути к папке содержимого класса
+    car_path = f'{IMAGE_PATH}{CLASS_LIST[i]}/'
+    # Выбор случайного фото из i-го класса
+    img_path = car_path + random.choice(os.listdir(car_path))
+    # Отображение фотографии (подробнее будет объяснено далее)
+    axs[i].set_title(CLASS_LIST[i])
+    axs[i].imshow(Image.open(img_path))
+    axs[i].axis('off')
+
+# Отрисовка всего полотна
+plt.show()
+
+
+# Создание списков файлов и их меток класса
+
+data_files = []                           # Cписок путей к файлам картинок
+data_labels = []                          # Список меток классов, соответствующих файлам
+
+for class_label in range(CLASS_COUNT):    # Для всех классов по порядку номеров (их меток)
+    class_name = CLASS_LIST[class_label]  # Выборка имени класса из списка имен
+    class_path = IMAGE_PATH + class_name  # Формирование полного пути к папке с изображениями класса
+    class_files = os.listdir(class_path)  # Получение списка имен файлов с изображениями текущего класса
+    print(f'Размер класса {class_name} составляет {len(class_files)} машин')
+
+    # Добавление к общему списку всех файлов класса с добавлением родительского пути
+    data_files += [f'{class_path}/{file_name}' for file_name in class_files]
+
+    # Добавление к общему списку меток текущего класса - их ровно столько, сколько файлов в классе
+    data_labels += [class_label] * len(class_files)
+
+print('Общий размер базы для обучения:', len(data_labels))
+
+print('Пути к файлам: ', data_files[1085:1090])
+print('Их метки классов:', data_labels[1085:1090])
+
+# Формирование набора данных из имеющейся базы
+
+# Задание единых размеров изображений
+
+IMG_WIDTH = 128                           # Ширина изображения
+IMG_HEIGHT = 64                           # Высота изображения
+
+data_images = []                          # Пустой список для данных изображений
+
+for file_name in data_files:
+    # Открытие и смена размера изображения
+    img = Image.open(file_name).resize((IMG_WIDTH, IMG_HEIGHT))
+    img_np = np.array(img)                # Перевод в numpy-массив
+    data_images.append(img_np)            # Добавление изображения в виде numpy-массива к общему списку
+
+x_data = np.array(data_images)            # Перевод общего списка изображений в numpy-массив
+y_data = np.array(data_labels)            # Перевод общего списка меток класса в numpy-массив
+
+print(f'В массив собрано {len(data_images)} фотографий следующей формы: {img_np.shape}')
+print(f'Общий массив данных изображений следующей формы: {x_data.shape}')
+print(f'Общий массив меток классов следующей формы: {y_data.shape}')
+
+x_data[0]
+
+# Нормированние массива изображений
+x_data = x_data / 255.
+
+# Создание сверточной сети
+
+# Создание модели последовательной архитектуры
+model = Sequential()
+
+# Первый сверточный блок
+model.add(Conv2D(256, (3, 3), name='First_C', padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)))  # (64,128,3) --> (64,128,256) меняется кол-во фильтров
+model.add(BatchNormalization(name='First_B'))  # (64,128,256) --> (64,128,256) нет изменений
+
+# Второй сверточный блок
+model.add(Conv2D(256, (3, 3), name='Second_C', padding='same', activation='relu'))  # (64,128,256) --> (64,128,256) нет изменений
+model.add(MaxPooling2D(pool_size=(3, 3), name='Second_M'))  # сжимает размер в три раза, с потерей, т.к. padding='valid', глубина не изм. (64,128,256) --> (21,42,256)
+
+# Третий сверточный блок
+model.add(Conv2D(256, (3, 3), name='Third_C', padding='same', activation='relu'))  # (21,42,256) --> (21,42,256) нет изменений
+model.add(BatchNormalization(name='Third_B'))  # (21,42,256) --> (21,42,256) нет изменений
+model.add(Dropout(0.2, name='Third_D'))  # (21,42,256) --> (21,42,256) нет изменений
+
+# Четвертый сверточный блок
+model.add(Conv2D(256, (3, 3), name='Fourth_C', padding='same', activation='relu'))  # (21,42,256) --> (21,42,256) нет изменений
+model.add(MaxPooling2D(pool_size=(3, 3), name='Fourth_M'))  # сжимает размер в три раза, хотя padding='valid', потерь нет, т.к. делится нацело, глубина не изм. (21,42,256) --> (7,14,256)
+model.add(Dropout(0.2, name='Fourth_D'))  # (7,14,256) --> (7,14,256) нет изменений
+
+# Пятый сверточный блок
+model.add(Conv2D(512, (3, 3), name='Fifth_D', padding='same', activation='relu'))  # (7,14,256) --> (7,14,512) меняется количество фильтров
+model.add(BatchNormalization(name='Fifth_B'))  # (7,14,512) --> (7,14,512) нет изменений
+
+# Шестой сверточный блок
+model.add(Conv2D(1024, (3, 3), name='Sixth_C', padding='same', activation='relu'))  # (7,14,1024) --> (7,14,1024) меняется кол-во фильтров
+model.add(MaxPooling2D(pool_size=(3, 3), name='Sixth_M'))  # сжимает размер в три раза, с потерей, т.к. padding='valid', глубина не изм. (7,14,1024) --> (2,4,1024)
+model.add(Dropout(0.2, name='Sixth_D'))  # (2,4,1024) --> (2,4,1024) нет изменений
+
+# Блок классификации
+model.add(Flatten(name='Class_1'))  # слой преобразования многомерных данных в одномерные (2,4,1024) --> (2*4*1024) --> (8192)
+model.add(Dense(2048, activation='relu', name='Class_2'))  # полносвязный слой, меняется кол-во нейронов (8192) --> (2048)
+model.add(Dense(4096, activation='relu', name='Class_3'))  # полносвязный слой, меняется кол-во нейронов (2048) --> (4096)
+model.add(Dense(CLASS_COUNT, activation='softmax', name='Class_4'))  # выходной полносвязный слой, кол-во нейронов = количество классов (4096) --> (3)
+
+model.summary()
+
+# Компиляция модели
+model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(learning_rate=0.0001), metrics=['accuracy'])
+
+# Обучение нейронной сети
+
+# Обучение модели сверточной нейронной сети подготовленных данных
+
+store_learning = model.fit(x_data,  # ----------------- x_train, примеры набора данных
+                           y_data,  # ----------------- y_train, метки примеров набора данных
+                           validation_split=0.2,  # --- 0.2 - доля данных для валидационной (проверочной) выборки, 1-0.2=0.8 останется в обучающей
+                           shuffle=True,  # ----------- перемешивание данных для равномерного обучения, соответствие экземпляра и метки сохраняется
+                           batch_size=25,  # ---------- размер пакета, который обрабатывает нейронка перед одним изменением весов
+                           epochs=35,  # -------------- epochs - количество эпох обучения
+                           verbose=1)  # -------------- 0 - не визуализировать ход обучения, 1 - визуализировать
+
+# Создание полотна для рисунка
+plt.figure(1, figsize=(18, 5))
+
+# Задание первой (левой) области для построения графиков
+plt.subplot(1, 2, 1)
+# Отрисовка графиков 'loss' и 'val_loss' из значений словаря store_learning.history
+plt.plot(store_learning.history['loss'],
+         label='Значение ошибки на обучающем наборе')
+plt.plot(store_learning.history['val_loss'],
+         label='Значение ошибки на проверочном наборе')
+# Задание подписей осей
+plt.xlabel('Эпоха обучения')
+plt.ylabel('Значение ошибки')
+plt.legend()
+
+# Задание второй (правой) области для построения графиков
+plt.subplot(1, 2, 2)
+# Отрисовка графиков 'accuracy' и 'val_accuracy' из значений словаря store_learning.history
+plt.plot(store_learning.history['accuracy'],
+         label='Доля верных ответов на обучающем наборе')
+plt.plot(store_learning.history['val_accuracy'],
+         label='Доля верных ответов на проверочном наборе')
+# Задание подписей осей
+plt.xlabel('Эпоха обучения')
+plt.ylabel('Доля верных ответов')
+plt.legend()
+
+# Фиксация графиков и рисование всей картинки
+plt.show()
